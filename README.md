e69ea6d6550c8fe3052cb184e4ec2e887fa389c5
Roman Grisch <rgrisch@gmail.com>
2023-04-03 13:40:58 +0200

    add file_reporter.py script

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## file reporter
+
+This python script asks for a directory path as input and will return the 20 largest files within that directory.
--- file_reporter.py
+++ file_reporter.py
@@ -1,1 +1,20 @@
-print('test')
+import os
+from os.path import abspath, join, getsize
+
+sizes = {}
+
+user_path = input("Enter directory path:")
+
+for top_dir, directories, files in os.walk(user_path):
+    for _file in files:
+        full_path = abspath(join(top_dir, _file))
+        size = getsize(full_path)
+        sizes[full_path] = size
+        
+
+sorted_results = sorted(sizes, key=sizes.get, reverse=True)
+
+for path in sorted_results[:20]:
+    print("Path: {0}, size: {1}".format(path, sizes[path]))
+
+    
